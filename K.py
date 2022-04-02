1e181fbab7b6c94ee21c304b659764d445dd6a3b
Your Name <you@example.com>
2022-04-02 15:04:45 -0400

    Adding K module and refactoring

---
+++ K.py
@@ -0,0 +1,4 @@
+URL_TEMPLATE = 'https://www.kimovil.com/en/where-to-buy-{}'
+
+ANTUTU_SCORE_SEL = '.item-antutu > a:nth-child(1) > span:nth-child(2)'
+ANTUTU_VERSION_SEL = '.item-antutu > a:nth-child(1) > span:nth-child(1)'
--- antutu.py
+++ antutu.py
@@ -1,25 +1,36 @@
 import requests_html
-
-ANTUTU_SCORE_SEL = '.item-antutu > a:nth-child(1) > span:nth-child(2)'
-URL = 'https://www.kimovil.com/en/where-to-buy-{}'
+import K
 
 def get_mobile_url(mobile_model: str) -> str:
     mobile_model_normalized = mobile_model.lower().replace(' ','-')
-    mobile_url = URL.format(mobile_model_normalized)
+    mobile_url = K.URL_TEMPLATE.format(mobile_model_normalized)
     return mobile_url
 
+
+def get_antutu_score(resp_html: requests_html.HTML,
+        antutu_score_sel: str = K.ANTUTU_SCORE_SEL ) -> int:
+    antutu_score_text = resp_html.find(antutu_score_sel, first=True).text
+    antutu_score = antutu_score_text.replace('.', '')
+    return int(antutu_score)
+
+
+def get_antutu_version(resp_html: requests_html.HTML,
+        antutu_version_sel: str = K.ANTUTU_VERSION_SEL ) -> str:
+    return resp_html.find(antutu_version_sel, first=True).text
+
+
 def get_antutu(mobile_model: str,
+        version_sel: str = K.ANTUTU_VERSION_SEL,
+        score_sel: str = K.ANTUTU_SCORE_SEL,
         ses: requests_html.HTMLSession = requests_html.HTMLSession()) -> int:
     mobile_url = get_mobile_url(mobile_model)
-    resp = ses.get(mobile_url)
-    antutu_score_str = resp.html.find(ANTUTU_SCORE_SEL, first=True).text
-    try:
-        antutu_score = int(antutu_score_str.replace('.',''))
-    except ValueError:
-        antutu_score = None
-    return antutu_score
+    resp_html = ses.get(mobile_url).html
+    version = get_antutu_version(resp_html, version_sel)
+    score = get_antutu_score(resp_html, score_sel)
+    antutu = { 'version': version, 'score': score}
+    return antutu
 
 
-ses = requests_html.HTMLSession()
-antutu_score = get_antutu('Samsung Galaxy A71', ses=ses)
-
+if __name__ == '__main__':
+    ses = requests_html.HTMLSession()
+    antutu = get_antutu('Samsung Galaxy A71')
