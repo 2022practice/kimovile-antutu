113bde7e5b987635978064c14a4d87d8bc9f8098
Your Name <you@example.com>
2022-04-02 13:00:19 -0400

    Adding get_mobile_url func; fixed get_antutu func

--- antutu.py
+++ antutu.py
@@ -1,18 +1,22 @@
 import requests_html
 
 ANTUTU_SCORE_SEL = '.item-antutu > a:nth-child(1) > span:nth-child(2)'
-URL = 'https://www.kimovil.com/en/where-to-buy-samsung-galaxy-a71'
+URL = 'https://www.kimovil.com/en/where-to-buy-{}'
 
 def get_mobile_url(mobile_model: str) -> str:
-    # TODO: get this code real!
-    return URL
+    mobile_model_normalized = mobile_model.lower().replace(' ','-')
+    mobile_url = URL.format(mobile_model_normalized)
+    return mobile_url
 
 def get_antutu(mobile_model: str,
         ses: requests_html.HTMLSession = requests_html.HTMLSession()) -> int:
     mobile_url = get_mobile_url(mobile_model)
-    resp = ses.get(URL)
+    resp = ses.get(mobile_url)
     antutu_score_str = resp.html.find(ANTUTU_SCORE_SEL, first=True).text
-    antutu_score = int(antutu_score_str.replace('.',''))
+    try:
+        antutu_score = int(antutu_score_str.replace('.',''))
+    except ValueError:
+        antutu_score = None
     return antutu_score
 
 
